59b10df15af1654c1c00ab9dd54b754059b0b604
Davide Guerri <davide.guerri@gmail.com>
2015-06-24 14:47:40 +0100

    Change keystone_service/_endpoint license to GPLv3
    Ansible modules are required to use GPLv3 or compatible license.

--- cloud/openstack/os_keystone_endpoint.py
+++ cloud/openstack/os_keystone_endpoint.py
@@ -4,19 +4,18 @@
 # Copyright (c) 2015 Hewlett-Packard Development Company, L.P.
 # Author: Davide Guerri <davide.guerri@hp.com>
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
 
 try:
     import shade
